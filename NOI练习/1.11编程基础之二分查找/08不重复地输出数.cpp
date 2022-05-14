3e133dd59329bcd7205def3fc218994a7495861e
EmptyBlue <lyt0112@stu.pku.edu.cn>
2022-05-15 01:13:49 +0800

    Update 08不重复地输出数.cpp

--- NOI练习/1.11编程基础之二分查找/08不重复地输出数.cpp
+++ NOI练习/1.11编程基础之二分查找/08不重复地输出数.cpp
@@ -2,17 +2,17 @@
 #include <set>
 using namespace std;
 int main() {
-	int n,tmp;
+	int n, tmp;
 	set<int> a;
-	cin>>n;
-	for(int i=0; i<n; i++) {
-		cin>>tmp;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> tmp;
 		a.insert(tmp);
 	}
-	n=a.size();
-	set<int>::iterator itr=a.begin();
-	for(int i=0; i<n; i++) {
-		cout<<*itr<<' ';
+	n = a.size();
+	set<int>::iterator itr = a.begin();
+	for (int i = 0; i < n; i++) {
+		cout << *itr << ' ';
 		itr++;
 	}
 }
