dbe0f382e7504f6bb9641562cb607d36410d2b44
EmptyBlue <lyt0112@stu.pku.edu.cn>
2022-05-15 01:13:04 +0800

    Update 07和为给定数.cpp

--- NOI练习/1.11编程基础之二分查找/07和为给定数.cpp
+++ NOI练习/1.11编程基础之二分查找/07和为给定数.cpp
@@ -4,22 +4,22 @@
 using namespace std;
 
 int main() {
-	ll n,tmp,aim;
+	ll n, tmp, aim;
 	multiset<ll> a;
-	cin>>n;
-	for(int i=0; i<n; i++) {
-		cin>>tmp;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> tmp;
 		a.insert(tmp);
 	}
-	cin>>aim;
-	set<ll>::iterator itr=a.begin(),end=a.end(),find;
-	for(ll i=0; i<n; i++) {
-		find=a.find(aim-*itr);
-		if(find!=end&&find!=itr) {//找到的不能是自己的迭代器，防止自己加自己是答案
-			cout<<*itr<<' '<<aim-*itr<<endl;
+	cin >> aim;
+	set<ll>::iterator itr = a.begin(), end = a.end(), find;
+	for (ll i = 0; i < n; i++) {
+		find = a.find(aim - *itr);
+		if (find != end && find != itr) {//找到的不能是自己的迭代器，防止自己加自己是答案
+			cout << *itr << ' ' << aim - *itr << endl;
 			return 0;
 		}
 		itr++;
 	}
-	cout<<"No"<<endl;
+	cout << "No" << endl;
 }
