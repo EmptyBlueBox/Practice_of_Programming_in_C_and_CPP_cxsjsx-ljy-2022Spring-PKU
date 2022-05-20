7547ea7ced907ae3e69c021d685d1c67616eecf5
EmptyBlue <lyt0112@stu.pku.edu.cn>
2022-05-20 22:41:26 +0800

    Add files via upload

---
+++ NOI练习/2.6基本算法之动态规划/4977怪盗基德的滑翔翼.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#define endl '\n'
+using namespace std;
+//最长上升（下降）不连续子串
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int N;
+	cin >> N;
+	while (N--) {
+		int n, ans = -1;
+		vector<int> h, dp1,dp2;
+		cin >> n;
+		h.resize(n), dp1.resize(n, 1), dp2.resize(n, 1);
+		for (int i = 0; i < n; i++)
+			cin >> h[i];
+		for (int i = 1; i < n; i++) {
+			for (int j = 0; j < i; j++) {
+				if (h[j] > h[i])
+					dp1[i] = max(dp1[i], dp1[j] + 1);
+				else if(h[j]<h[i])
+					dp2[i] = max(dp2[i], dp2[j] + 1);
+			}
+			ans = max(ans, dp1[i]);//每循环一个就维护，不能直接ans = max(ans, dp1[n-1])。因为是以它为终点而不是在它之前最长的
+			ans = max(ans, dp2[i]);
+		}
+		cout << ans << endl;
+	}
+}
