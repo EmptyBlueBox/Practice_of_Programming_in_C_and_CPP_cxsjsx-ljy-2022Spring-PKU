4ccc7c527e5ecae68b40ffb2be574e37562cd5cd
EmptyBlueBox <lyt0112@stu.pku.edu.cn>
2022-06-11 01:42:58 +0800

    不是背包问题，从询问的不是最大价值，而是背包最少数量可以看出来

---
+++ 2022 程序设计实习周末上机练习/上机练习06（算法部分）/G 忍者道具.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int n, c, w[19], bag_used[19] = { 0 }, ans = 0x3f3f3f3f;
+void dfs(int piece) {
+	if (bag_used[0] > ans)//已经用了比已知答案多，prune
+		return;
+	if (piece == n + 1) {
+		ans = min(ans, bag_used[0]);
+		return;
+	}
+	for (int i = 1; i <= bag_used[0]; i++)
+		if (w[piece] + bag_used[i] <= c) {
+			bag_used[i] += w[piece];
+			dfs(piece + 1);
+			bag_used[i] -= w[piece];
+		}
+	bag_used[0]++;
+	bag_used[bag_used[0]] = w[piece];
+	dfs(piece + 1);
+	bag_used[bag_used[0]] = 0;
+	bag_used[0]--;
+}
+int main() {
+	cin >> n >> c;
+	for (int i = 1; i <= n; i++)
+		cin >> w[i];
+	bag_used[0] = 1;//现在用了一个
+	dfs(1);
+	cout << ans << endl;
+}
