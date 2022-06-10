3f6be9c3d38376cc8dab332a9fd5bd3170e47ae6
EmptyBlueBox <lyt0112@stu.pku.edu.cn>
2022-06-11 01:17:12 +0800

    先dfs出所有五位数再查询，不是dp

---
+++ 2022 程序设计实习周末上机练习/上机练习06（算法部分）/F 数字变换.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <queue>
+using namespace std;
+
+struct node {
+	string a;
+	int step, t2, t3;
+	node(string _a, int _step, int _t2, int _t3) :a(_a), step(_step), t2(_t2), t3(_t3) {}
+};
+int ans[100000], n;
+int main() {
+	memset(ans, 0x3f, sizeof(ans));
+	ans[12345] = 0;
+	queue<node> q;
+	q.push({ "12345",0,0,0 });
+	while (!q.empty()) {
+		node now = q.front();
+		q.pop();
+		string ss = now.a;
+		for (int i = 0; i < 5; i++)
+			for (int j = i + 1; j < 5; j++) {
+				swap(ss[i], ss[j]);
+				int tmp = atoi(ss.c_str());
+				if (ans[tmp] == 0x3f3f3f3f) {
+					q.push({ ss,now.step + 1,now.t2,now.t3 });
+					ans[tmp] = now.step + 1;
+				}
+				swap(ss[i], ss[j]);
+			}
+		if (now.t2 < 3)
+			for (int i = 0; i < 5; i++) {
+				ss[i] = char((ss[i] - '0' + 1) % 10 + '0');
+				int tmp = atoi(ss.c_str());
+				if (ans[tmp] == 0x3f3f3f3f) {
+					q.push({ ss,now.step + 1,now.t2 + 1,now.t3 });
+					ans[tmp] = now.step + 1;
+				}
+				ss = now.a;
+			}
+		if (now.t3 < 2)
+			for (int i = 0; i < 5; i++) {
+				ss[i] = char(((ss[i] - '0') * 2) % 10 + '0');
+				int tmp = atoi(ss.c_str());
+				if (ans[tmp] == 0x3f3f3f3f) {
+					q.push({ ss,now.step + 1,now.t2,now.t3 + 1 });
+					ans[tmp] = now.step + 1;
+				}
+				ss = now.a;
+			}
+	}
+	while (cin >> n)
+		cout << (ans[n] == 0x3f3f3f3f ? -1 : ans[n]) << endl;
+}
