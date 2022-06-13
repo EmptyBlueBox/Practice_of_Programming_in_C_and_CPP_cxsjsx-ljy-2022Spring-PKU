4c80fb32cff1e9f2357d5ed434e0e68d0aeaa04a
EmptyBlueBox <lyt0112@stu.pku.edu.cn>
2022-06-13 17:00:36 +0800

    Update E Project Summer.cpp

--- 2022 程序设计实习周末上机练习/上机练习06（算法部分）/E Project Summer.cpp
+++ 2022 程序设计实习周末上机练习/上机练习06（算法部分）/E Project Summer.cpp
@@ -1,46 +1,55 @@
 #include <iostream>
+#include <cstring>
 #include <vector>
 #include <queue>
 using namespace std;
 
-struct node {
-	int x, y, t;
-	node(int _x, int _y, int _t) :x(_x), y(_y), t(_t) {}
-	node(pair<int, int> p, int _t) :x(p.first), y(p.second), t(_t) {}
-};
 int main() {
 	int N, T, dx[4] = { 0,1,0,-1 }, dy[4] = { 1,0,-1,0 };
 	cin >> N;
-	T = N;
-	while (N--) {
-		printf("Case #%d: ", T - N);
-		int m, n, vis[102][102] = { {false} };
-		char map[102][102];
-		queue<node> q;
-		vector<pair<int, int>> tp[26];//传送门坐标
+	for (int T = 1; T <= N; T++) {
+		printf("Case #%d: ", T);
+		int m, n, dis[102][102];
 		cin >> m >> n;
+		memset(dis, 0x3f, sizeof(dis));
+		char map[102][102];
+		queue<pair<int, int>> q;
+		vector<pair<int, int>> tp[26];//传送门坐标
+		pair<int, int> start, end;
 		for (int i = 1; i <= m; i++)
 			for (int j = 1; j <= n; j++) {
 				cin >> map[i][j];
-				if (map[i][j] == 'B') q.push({ i,j,0 }), map[i][j] = '.';
-				if (map[i][j] >= 'a' && map[i][j] <= 'z') tp[map[i][j] - 'a'].push_back({ i,j });
+				if (map[i][j] == 'B') start = make_pair(i, j), map[i][j] = '.';
+				else if (map[i][j] == 'I') end = make_pair(i, j), map[i][j] = '.';
+				else if (map[i][j] >= 'a' && map[i][j] <= 'z') tp[map[i][j] - 'a'].push_back({ i,j });
 			}
+		q.push(start), dis[start.first][start.second] = 0;
 		while (!q.empty()) { //走四个方向，如果now是传送门就加钻洞
-			node now = q.front();
+			pair<int, int> now = q.front();
 			q.pop();
-			//判断是否撞墙、到过、越界可以在最开始进行
-			if (map[now.x][now.y] == '#' || vis[now.x][now.y] || now.x<1 || now.x>m || now.y<1 || now.y>n)
-				continue;
-			if (map[now.x][now.y] == 'I') {
-				cout << now.t << endl;
-				goto loop;
-			}
-			vis[now.x][now.y] = true;
-			if (map[now.x][now.y] >= 'a' && map[now.x][now.y] <= 'z')
-				q.push(tp[map[now.x][now.y] - 'a'][0] == make_pair(now.x, now.y) ? node{ tp[map[now.x][now.y] - 'a'][1], now.t + 1 } : node{ tp[map[now.x][now.y] - 'a'][0], now.t + 1 });
+			int xx, yy;
 			for (int i = 0; i < 4; i++) {
-				int xx = now.x + dx[i], yy = now.y + dy[i];
-				q.push({ xx, yy, now.t + 1 });
+				xx = now.first + dx[i], yy = now.second + dy[i];
+				if (dis[xx][yy] != 0x3f3f3f3f || map[xx][yy] == '#' || xx<1 || xx>m || yy<1 || yy>n)
+					continue;
+				else if (end == make_pair(xx, yy)) {
+					cout << dis[now.first][now.second] + 1 << endl;
+					goto loop;
+				}
+				dis[xx][yy] = dis[now.first][now.second] + 1;
+				q.push({ xx, yy });
+			}
+			if (map[now.first][now.second] >= 'a' && map[now.first][now.second] <= 'z') {
+				xx = (tp[map[now.first][now.second] - 'a'][0] == now ? tp[map[now.first][now.second] - 'a'][1].first : tp[map[now.first][now.second] - 'a'][0].first);
+				yy = (tp[map[now.first][now.second] - 'a'][0] == now ? tp[map[now.first][now.second] - 'a'][1].second : tp[map[now.first][now.second] - 'a'][0].second);
+				if (dis[xx][yy] != 0x3f3f3f3f || map[xx][yy] == '#' || xx<1 || xx>m || yy<1 || yy>n)
+					continue;
+				else if (end == make_pair(xx, yy)) {
+					cout << dis[now.first][now.second] + 1 << endl;
+					goto loop;
+				}
+				dis[xx][yy] = dis[now.first][now.second] + 1;
+				q.push({ xx, yy });
 			}
 		}
 		cout << -1 << endl;
