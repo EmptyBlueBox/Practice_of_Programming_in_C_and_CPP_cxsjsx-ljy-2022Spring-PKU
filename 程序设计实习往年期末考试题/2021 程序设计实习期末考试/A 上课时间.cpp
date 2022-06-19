f0a796b5f56ef88ec630a3e00037cb5ca4cde443
EmptyBlueBox <lyt0112@stu.pku.edu.cn>
2022-06-19 19:51:50 +0800

    Update A 上课时间.cpp

--- 程序设计实习往年期末考试题/2021 程序设计实习期末考试/A 上课时间.cpp
+++ 程序设计实习往年期末考试题/2021 程序设计实习期末考试/A 上课时间.cpp
@@ -15,29 +15,3 @@ int main() {
 				ans = max(ans, min(a[j].second, a[i].second) - a[j].first + 1);
 	cout << ans << endl;
 }
-
-//WA
-/*
-#include <iostream>
-using namespace std;
-
-int main() {
-	typedef pair<int, int> pii;
-	int n, ans = 0xc0c0c0c0;//不能初始化为最小值，有可能所有课都不冲突！！！
-	pii a[1001];
-	cin >> n;
-	for (int i = 1; i <= n; i++)
-		cin >> a[i].first >> a[i].second;
-	for (int i = 1; i <= n; i++)
-		for (int j = i + 1; j <= n; j++)
-			if (a[i].first <= a[j].first && a[i].second >= a[j].second)
-				ans = max(ans, a[j].second - a[j].first + 1);
-			else if (a[j].first <= a[i].first && a[j].second >= a[i].second)
-				ans = max(ans, a[i].second - a[i].first + 1);
-			else if (a[i].first <= a[j].first && a[i].second >= a[j].first)
-				ans = max(ans, a[i].second - a[j].first + 1);
-			else if (a[j].first <= a[i].first && a[j].second >= a[i].first)
-				ans = max(ans, a[j].second - a[i].first + 1);
-	cout << ans << endl;
-}
-*/
