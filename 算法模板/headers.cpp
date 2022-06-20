fe3f4036f560feb89147d5c38412f83146fca676
EmptyBlueBox <lyt0112@stu.pku.edu.cn>
2022-06-21 03:10:06 +0800

    Create headers.cpp

---
+++ 算法模板/headers.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <queue>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <bitset>
+#include <algorithm>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false), cin.tie(0);
+	
+}
