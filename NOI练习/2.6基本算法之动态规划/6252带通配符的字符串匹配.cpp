1e0ca8dee25ae009f041847c8a9e83eb70bbb23f
EmptyBlueBox <lyt0112@stu.pku.edu.cn>
2022-06-23 19:13:20 +0800

    reviewing

--- NOI练习/2.6基本算法之动态规划/6252带通配符的字符串匹配.cpp
+++ NOI练习/2.6基本算法之动态规划/6252带通配符的字符串匹配.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
+#define int long long
 using namespace std;
 
-int main() {
+signed main() {
 	string a, b;
 	cin >> a >> b;
 	int lena = a.length(), lenb = b.length();
@@ -11,14 +12,13 @@ int main() {
 	if (a[1] == '*')
 		dp[1][0] = true;//注意初始化：a只有*，b什么都没有是可以匹配的，而a只有？；b什么都没有是不可以匹配的
 	for (int i = 1; i <= lena; i++)
-		for (int j = 1; j <= lenb; j++) {
+		for (int j = 1; j <= lenb; j++)
 			if (a[i] == '?')
 				dp[i][j] = dp[i - 1][j - 1];
 			else if (a[i] == '*')
 				dp[i][j] = dp[i - 1][j] | dp[i][j - 1];//不用来匹配或用来匹配
 			else if (a[i] == b[j])
 				dp[i][j] = dp[i - 1][j - 1];
-		}
 	if (dp[lena][lenb])
 		cout << "matched" << endl;
 	else
