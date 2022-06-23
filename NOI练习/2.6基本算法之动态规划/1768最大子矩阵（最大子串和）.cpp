9233671a0168087f677688665f9100ffc5d8bd8a
EmptyBlueBox <lyt0112@stu.pku.edu.cn>
2022-06-23 15:46:42 +0800

    reviewing

---
+++ NOI练习/2.6基本算法之动态规划/1768最大子矩阵（最大子串和）.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int n, a[101][101];
+//把每一行加起来
+//动态规划寻找最大连续子序列和
+inline int Max_Sum(int* sum) {
+	int dp[101] = { 0 };
+	int ans = 0xc0c0c0c0;
+	for (int i = 1; i <= n; i++) {
+		dp[i] = max(dp[i - 1] + sum[i], sum[i]);
+		ans = max(ans, dp[i]);
+	}
+	return ans;
+}
+int main() {
+	cin >> n;
+	if (n == 1) {
+		cin >> n;
+		cout << n;
+		return 0;
+	}
+	for (int i = 1; i <= n; i++)
+		for (int j = 1; j <= n; j++)
+			cin >> a[i][j];
+	int ans = 0xc0c0c0c0;
+	for (int i = 1; i <= n; i++) {
+		int sum[101] = { 0 };
+		for (int j = i; j <= n; j++) {//从第i行加n-i次,每次加第j行
+			for (int k = 1; k <= n; k++)
+				sum[k] += a[j][k];
+			ans = max(ans, Max_Sum(sum));
+		}
+	}
+	cout << ans << endl;
+}
