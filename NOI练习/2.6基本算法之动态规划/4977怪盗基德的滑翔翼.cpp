fbae902aa20011dc624de853d7d7fa40440ea873
EmptyBlueBox <lyt0112@stu.pku.edu.cn>
2022-06-23 18:11:26 +0800

    reviewing

--- NOI练习/2.6基本算法之动态规划/4977怪盗基德的滑翔翼.cpp
+++ NOI练习/2.6基本算法之动态规划/4977怪盗基德的滑翔翼.cpp
@@ -4,26 +4,23 @@
 #define endl '\n'
 using namespace std;
 
-//取最长上升子序列与最长下降子序列更长的那个
-int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(0);
+int main() {//取最长上升子序列与最长下降子序列更长的那个
+	ios::sync_with_stdio(false), cin.tie(0);
 	int N;
 	cin >> N;
 	while (N--) {
 		int n, ans = -1;
-		vector<int> h, dp1,dp2;
+		vector<int> h, dp1, dp2;
 		cin >> n;
 		h.resize(n), dp1.resize(n, 1), dp2.resize(n, 1);
 		for (int i = 0; i < n; i++)
 			cin >> h[i];
 		for (int i = 1; i < n; i++) {
-			for (int j = 0; j < i; j++) {
+			for (int j = 0; j < i; j++)
 				if (h[j] > h[i])
 					dp1[i] = max(dp1[i], dp1[j] + 1);
-				else if(h[j]<h[i])
+				else if (h[j] < h[i])
 					dp2[i] = max(dp2[i], dp2[j] + 1);
-			}
 			ans = max(ans, dp1[i]);//每循环一个就维护，不能直接ans = max(ans, dp1[n-1])。因为是以它为终点而不是在它之前最长的
 			ans = max(ans, dp2[i]);
 		}
