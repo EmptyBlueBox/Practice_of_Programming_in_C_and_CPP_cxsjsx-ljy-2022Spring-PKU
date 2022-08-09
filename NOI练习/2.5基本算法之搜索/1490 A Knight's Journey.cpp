8ea32f5c8a83ceb17e61316e325066078850d738
EmptyBlueBox <lyt0112@stu.pku.edu.cn>
2022-08-10 00:39:21 +0800

    Update 1490 A Knight's Journey.cpp

--- NOI练习/2.5基本算法之搜索/1490 A Knight's Journey.cpp
+++ NOI练习/2.5基本算法之搜索/1490 A Knight's Journey.cpp
@@ -5,22 +5,26 @@
 using namespace std;
 
 //一定可以从(1, 1)开始，按字典序深搜得到答案
-//DFS的参数都可以变成全局变量，要恰当决定哪些变成全局变量
+// DFS的参数都可以变成全局变量，要恰当决定哪些变成全局变量
 //比如这个题，一些题解的第三个参数是与(1, 1)的距离，我们可以把他与全局变量的防止重复搜索的vis数组合并，简化DFS的参数
-int m, n, dx[8] = { -1,1,-2,2,-2,2,-1,1 }, dy[8] = { -2,-2,-1,-1,1,1,2,2 }, dis[28][28] = { {0} };//dis是vis与距(1, 1)距离的数组合并得到的
+int m, n, dx[8] = {-1, 1, -2, 2, -2, 2, -1, 1}, dy[8] = {-2, -2, -1, -1, 1, 1, 2, 2}, dis[28][28] = {{0}}; // dis是vis与距(1, 1)距离的数组合并得到的
 vector<string> ans;
 bool all_end = false;
-void dfs(int x, int y) {
-	if (dis[x][y] == m * n - 1) {
+void dfs(int x, int y)
+{
+	if (dis[x][y] == m * n - 1)
+	{
 		for (auto i : ans)
 			cout << i;
-		cout << endl << endl;
+		cout << endl
+			 << endl;
 		all_end = true;
 		return;
 	}
-	for (int i = 0; i < 8; i++) {
+	for (int i = 0; i < 8; i++)
+	{
 		int xx = x + dx[i], yy = y + dy[i];
-		if (xx<1 || xx>m || yy<1 || yy>n || (dis[xx][yy] != 0x3f3f3f3f))
+		if (xx < 1 || xx > m || yy < 1 || yy > n || (dis[xx][yy] != 0x3f3f3f3f))
 			continue;
 		dis[xx][yy] = dis[x][y] + 1;
 		ans.push_back(char(yy - 1 + 'A') + to_string(xx));
@@ -31,10 +35,12 @@ void dfs(int x, int y) {
 		dis[xx][yy] = 0x3f3f3f3f;
 	}
 }
-int main() {
+int main()
+{
 	int N;
 	cin >> N;
-	for (int T = 1; T <= N; T++) {
+	for (int T = 1; T <= N; T++)
+	{
 		printf("Scenario #%d:\n", T);
 		memset(dis, 0x3f, sizeof(dis));
 		ans.clear();
